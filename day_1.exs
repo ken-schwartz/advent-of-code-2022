b1286ca9570ab71ef908063e44803ed633f98f33
Ken Schwartz <ken.schwartz@beam.dental>
2022-12-02 00:18:52 -0500

    day 1

---
+++ day_1.exs
@@ -0,0 +1,26 @@
+defmodule Day1 do
+  def sum_calorie_strings(list) do
+    list
+    |> Enum.map(&String.to_integer/1)
+    |> Enum.sum()
+  end
+end
+
+{:ok, all_elfs_snacks} = File.read("day_1_part_1.txt")
+
+sorted_total_calories =
+  all_elfs_snacks
+  |> String.split("\n\n")
+  |> Enum.map(&String.split/1)
+  |> Enum.map(&Day1.sum_calorie_strings/1)
+  |> Enum.sort()
+  |> Enum.reverse()
+
+# part one
+[head | _] = sorted_total_calories
+IO.puts("Part 1: #{head}")
+
+# part two
+[first, second, third | _] = sorted_total_calories
+top_three_calories = Enum.sum([first, second, third])
+IO.puts("Part 2: #{top_three_calories}")
